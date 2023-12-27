d4d709ccfc02f434ee30d682ad950ddf358134a7
busrapinar <bsrapnrr@gmail.com>
2023-12-27 18:53:35 +0300

    x clone project

---
+++ src/layouts/main/rightbar/index.jsx
@@ -0,0 +1,10 @@
+
+const RightSidebar = () => {
+  return (
+    <aside className="w-[350px] mr-2.5">
+        sağ side bar
+    </aside>
+  )
+}
+
+export default RightSidebar
---
+++ src/pages/home/index.jsx
@@ -0,0 +1,12 @@
+
+const Home = () => {
+  return (
+    <div className="">
+     
+        Home Component
+      
+    </div>
+  )
+}
+
+export default Home;

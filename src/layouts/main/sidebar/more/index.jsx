5e8691af0560b5e688d82bb4843fd7e15f571d6f
busrapinar <bsrapnrr@gmail.com>
2024-01-09 23:44:56 +0300

    redux-toolkit kurulum

--- src/layouts/main/sidebar/more/index.jsx
+++ src/layouts/main/sidebar/more/index.jsx
@@ -9,7 +9,7 @@ export default function More() {
 				<div
 					className="p-3 rounded-full transition-colors inline-flex items-center gap-5 group-hover:bg-[#eff3f41a]">
 					<div className="w-[26.25px] h-[26.25px] relative">
-						<svg viewBox="0 0 24 24" width={26.25} height={26.25}>
+						<svg viewBox="0 0 24 24" width={24} height={24}>
 							<path
 								fill="#e7e9ea"
 								d="M3.75 12c0-4.56 3.69-8.25 8.25-8.25s8.25 3.69 8.25 8.25-3.69 8.25-8.25 8.25S3.75 16.56 3.75 12zM12 1.75C6.34 1.75 1.75 6.34 1.75 12S6.34 22.25 12 22.25 22.25 17.66 22.25 12 17.66 1.75 12 1.75zm-4.75 11.5c.69 0 1.25-.56 1.25-1.25s-.56-1.25-1.25-1.25S6 11.31 6 12s.56 1.25 1.25 1.25zm9.5 0c.69 0 1.25-.56 1.25-1.25s-.56-1.25-1.25-1.25-1.25.56-1.25 1.25.56 1.25 1.25 1.25zM13.25 12c0 .69-.56 1.25-1.25 1.25s-1.25-.56-1.25-1.25.56-1.25 1.25-1.25 1.25.56 1.25 1.25z"
@@ -108,9 +108,13 @@ export default function More() {
                     )}
 				</Disclosure>
 				<Disclosure>
+				{({open}) => (
+					<div>
 					<Disclosure.Button className="h-[52px] flex items-center justify-between w-full px-4 font-bold hover:bg-[#eff3f41a] transition-colors">
 						Ayarlar ve Destek
-						<svg viewBox="0 0 24 24" width={18.75} height={18.75}>
+						<svg className={classNames({
+                            "rotate-180": open
+                        })} viewBox="0 0 24 24" width={18.75} height={18.75}>
 							<path
 								fill="currentColor"
 								d="M3.543 8.96l1.414-1.42L12 14.59l7.043-7.05 1.414 1.42L12 17.41 3.543 8.96z"
@@ -155,6 +159,8 @@ export default function More() {
 							Klavye kısayolları
 						</Link>
 					</Disclosure.Panel>
+					</div>
+                    )}
 				</Disclosure>
 			</Popover.Panel>
 		</Popover>
